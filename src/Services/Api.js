678b33fc36e75ac01432f33ebbd606152d52a13f
Bikash Das <99291618+bikashd003@users.noreply.github.com>
2024-03-04 22:00:16 +0530

    Update Api.js

--- src/Services/Api.js
+++ src/Services/Api.js
@@ -1,2 +1,2 @@
-export const API="https://invoice-generator-server-pte0.onrender.com";
+export const API="https://quiz-app-backend-hc5g.onrender.com/api";
 // export const API="http://localhost:8000/api"
